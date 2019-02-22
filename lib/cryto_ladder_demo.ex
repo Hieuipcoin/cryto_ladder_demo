963d8177374d800cdebc074d1962fa7229818808
Riki <hieult@ipcoin.vn>
2019-02-22 11:11:58 +0700

    first commit

---
+++ lib/cryto_ladder_demo.ex
@@ -0,0 +1,18 @@
+defmodule CrytoLadderDemo do
+  @moduledoc """
+  Documentation for CrytoLadderDemo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CrytoLadderDemo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cryto_ladder_demo_test.exs
@@ -0,0 +1,8 @@
+defmodule CrytoLadderDemoTest do
+  use ExUnit.Case
+  doctest CrytoLadderDemo
+
+  test "greets the world" do
+    assert CrytoLadderDemo.hello() == :world
+  end
+end
